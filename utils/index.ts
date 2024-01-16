3e6e430e14a13fe6d1bb4d1fba25e338bd095273
Jay Hwang <octave08b@gmail.com>
2024-01-16 13:00:18 +0900

    Fix build error

--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,10 @@
-export const yup = (tag) => (d) => {
+export const yup = (tag: string) => (d: any) => {
   // eslint-disable-next-line no-console
   console.log(`${tag}`, d);
   return d;
 };
 
-export const nope = (tag) => (d) => {
+export const nope = (tag: string) => (d: any) => {
   // eslint-disable-next-line no-console
   console.error(`Oh No!! [${tag}]`, d);
   return d;
